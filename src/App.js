b427b218e771708f0c6140535df0903c583543a0
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-09-27 14:58:11 +0200

    Add rules panel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 
 import Scoreboard from './components/Scoreboard';
 import Options from './components/Options';
 import Rules from './components/Rules';
 
 function App() {
+
+  const [rulesActive, setRulesActive] = useState(false);
+
+  const handleRulesPanel = () => {
+    setRulesActive(!rulesActive);
+  }
+
   return (
     <div className="App">
       <Scoreboard/>
       <Options/>
 
-      <button className="rules__btn">RULES</button>
-      <Rules/>
+      <button 
+      className="rules__btn"
+      onClick={handleRulesPanel}
+      >RULES</button>
+
+      {rulesActive ? <Rules handleRulesPanel={handleRulesPanel}/> : null}
+
     </div>
   );
 }
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,12 +4,16 @@ import "./Rules.css";
 import rulesetImg from "../images/image-rules.svg";
 import closeBtn from "../images/icon-close.svg";
 
-const Rules = () => {
+const Rules = ({handleRulesPanel}) => {
     return ( 
         <div className="rules">
             <div className="rules__panel">
                 <h1 className="rules__panel--header">RULES</h1>
-                <img src={closeBtn} alt="close" className="rules__panel--closeBtn" />
+                <img src={closeBtn}
+                alt="close"
+                className="rules__panel--closeBtn"
+                onClick={handleRulesPanel}
+                />
                 <img src={rulesetImg} alt="ruleset" className="rules__panel--img" />
             </div>
         </div>
