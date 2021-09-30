202055c2e83e5cbeb40c9f41bad1c6760de32293
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-09-30 17:22:16 +0200

    Add styles for game panel

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Game.css';
 
+import Token from './Token';
+
 const Game = () => {
     return ( 
         <div className="game">
-            Game
+
+            <div className="game__pick game__pick--player">
+                <h3 className="game__title game__title--player">YOU PICKED</h3>
+                <div className="game__token">
+                    <Token type="paper" className="game__token"/>
+                </div>
+            </div>
+
+            <div className="game__pick game__pick--house">
+                <h3 className="game__title game__title--house">THE HOUSE PICKED</h3>
+                <div className="game__token">
+                    <Token type="rock" className="game__token"/>
+                </div>
+            </div>
+
         </div>
      );
 }
